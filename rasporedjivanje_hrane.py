f2700660f745079ef12ecca04f625c34c5a9de8f
Ahmed Habibovic <ahmed.habibovic.25@size.ba>
2026-05-17 13:30:21 +0200

    Implementiran SJF non-preemptive algoritam i tabela prikaza rezultata

--- rasporedjivanje_hrane.py
+++ rasporedjivanje_hrane.py
@@ -1,7 +1,69 @@
 import os
 
+def prikazi_tabelu(podaci):
+    print("\n#Br.Narudžbe | T-Dolaska | T-Pripreme | T-Kompletiranja | Ukupno-TAT | T-Čekanja")
+    print("-" * 85)
+    
+    ukupni_tat = 0
+    ukupno_cekanje = 0
+    n = len(podaci)
+
+    
+    
+    for p in podaci:
+        print(f"{p['id']:<12} | {p['at']:<9} | {p['bt']:<10} | {p['ct']:<14} | {p['tat']:<10} | {p['wt']}")
+        ukupni_tat += p['tat']
+        ukupno_cekanje += p['wt']
+        
+    
+    print("-" * 85)
+    print(f"Prosječno vrijeme čekanja = {ukupno_cekanje / n:.2f} (min)")
+    print(f"Prosječno ukupno vrijeme izvršavanja (TAT) = {ukupni_tat / n:.2f} (min)")
+    input("\nPress any key to continue...")
+
+
+
+def sjf_non_preemptive():
+    print("\n--- SJF Non-Preemptive (Dostava hrane) ---")
+    n = int(input("Unesite ukupan broj narudžbi: "))
+    procesi = []
+    
+    for i in range(n):
+        print(f"Narudžba {i+1}:")
+        at = int(input("  Vrijeme dolaska/dobivanja narudžbe: "))
+        bt = int(input("  Vrijeme pripremanja (izraženo u minutama): "))
+        procesi.append({'id': i+1, 'at': at, 'bt': bt, 'zavrsen': False})
+
+    vrijeme = 0
+    zavrseni_broj = 0
+    rezultati = []
+
+    while zavrseni_broj < n:
+        kandidati = [p for p in procesi if p['at'] <= vrijeme and not p['zavrsen']]
+        
+        if not kandidati:
+            vrijeme += 1
+            continue
+        
+        odabrani = min(kandidati, key=lambda x: x['bt'])
+        
+        odabrani['zavrsen'] = True
+        vrijeme += odabrani['bt']
+        
+        odabrani['ct'] = vrijeme
+        odabrani['tat'] = odabrani['ct'] - odabrani['at']
+        odabrani['wt'] = odabrani['tat'] - odabrani['bt']
+        
+        rezultati.append(odabrani)
+        zavrseni_broj += 1
+    
+    rezultati.sort(key=lambda x: x['id'])
+    prikazi_tabelu(rezultati)
+
+
+
+
 def main():
-    """Glavni meni aplikacije."""
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("====================================================")
@@ -16,8 +78,7 @@ def main():
         izbor = input("\nVaš izbor: ")
         
         if izbor == '1':
-            print("Opcija 1 odabrana (U izradi...)")
-            input("Press enter...")
+            sjf_non_preemptive()
         elif izbor == '2':
             print("Opcija 2 odabrana (U izradi...)")
             input("Press enter...")
